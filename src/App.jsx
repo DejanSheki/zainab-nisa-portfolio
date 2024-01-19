e77749ceaf7d5737b9b3fa04367d073217bac2ed
DejanSheki <dejan.sheki.lukic@gmail.com>
2024-01-19 18:56:32 +0100

    starting new project

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <main className='max-w-[90rem] bg-white m-auto px-28'>
+      <div className='bg-bg bg-repeat bg-[length:5.999999865889549px_5.999999865889549px] border-l-[3px] border-black-100 h-screen'>
+        <h1 className='text-3xl text-pink-300 font-handlee'>Hello</h1>
+        <h1 className='text-3xl text-pink-300 font-inter'>Hello</h1>
+      </div>
+
+    </main>
+  )
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,55 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        'inter': ['Inter'],
+        'handlee': ['Handlee']
+      },
+      colors: {
+        'black': {
+          100: 'rgba(0, 0, 0, 1)',
+          200: 'rgba(12, 12, 12, 1)',
+          300: 'rgba(15, 15, 15, 1)'
+        },
+        'pink': {
+          100: 'rgba(255, 201, 240, 1)',
+          200: 'rgba(240, 114, 205, 1)',
+          300: 'rgba(243, 132, 212, 1)',
+          400: 'rgba(252, 117, 255, 1)',
+          500: 'rgba(219, 74, 144, 1)',
+        },
+        'yellow': {
+          100: 'rgba(255, 230, 140, 1)',
+          200: 'rgba(229, 193, 65, 1)',
+          300: 'rgba(238, 205, 86, 1)',
+          400: 'rgba(187, 156, 42, 1)',
+          500: 'rgba(248, 208, 68, 1)',
+        },
+        'blue': {
+          100: 'rgba(157, 218, 219, 1)',
+          200: 'rgba(157, 220, 255, 1)',
+          300: 'rgba(90, 181, 232, 1)',
+          400: 'rgba(68, 226, 248, 1)',
+          500: 'rgba(28, 106, 177, 1)',
+        },
+        'black-opacity': 'rgba(0, 0, 0, 0.06)',
+        'white': 'rgba(255, 255, 255, 1)',
+        'light-grey': 'rgba(179, 179, 179, 1)',
+        'grey': 'rgba(112, 111, 111, 1)',
+        'lihgt-green': 'rgba(124, 240, 114, 1)',
+        'green': 'rgba(78, 213, 67, 1)',
+        'orange': 'rgba(255, 87, 87, 1)'
+      },
+      backgroundImage: {
+        'bg': 'url("/bg-dot.png")'
+      }
+    },
+  },
+  plugins: [],
+}
+
