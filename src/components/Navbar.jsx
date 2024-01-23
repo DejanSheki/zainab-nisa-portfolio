f254b109edf4c213439c5b0ce1b0dcc713126976
DejanSheki <dejan.sheki.lukic@gmail.com>
2024-01-23 22:41:15 +0100

    ....

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,16 +1,28 @@
-import vector from '../assets/vector.svg';
+import vector from "../assets/vector.svg";
 
 const Navbar = () => {
   return (
     <nav className="bg-transparent flex justify-end items-center pt-12">
-      <div className='inline-flex items-center gap-4 mr-40'>
-        <img className='w-12' src={vector} alt="vector" />
-        <strong className='font-handlee font-extrabold text-4xl text-black-200'>znisa</strong>
+      <div className="inline-flex items-center gap-4 mr-40">
+        <img className="w-12" src={vector} alt="vector" />
+        <strong className="font-handlee font-medium text-4xl text-black-200">
+          znisa
+        </strong>
       </div>
-      <div className='inline-flex items-center'>
-        <a className='text-black-200 text-2xl font-normal font-inter mr-8' href="#">Portfolio</a>
-        <div className=' bg-gradient-to-t from-pink-100 from-30% to-transparent to-30% bg-[center_-6px] hover:from-pink-100 hover:from-60% hover:to-transparent hover:to-60% hover:bg-[center_-6px] h-min mr-1 pr-[1px] items-center'>
-          <a className='text-black-200 font-inter font-normal text-2xl' href="#">Hire Me</a>
+      <div className="inline-flex items-center">
+        <a
+          className="text-black-200 text-2xl font-normal font-inter mr-8"
+          href="#"
+        >
+          Portfolio
+        </a>
+        <div className=" bg-gradient-to-t from-pink-100 from-30% to-transparent to-30% bg-[center_-6px] hover:from-pink-100 hover:from-60% hover:to-transparent hover:to-60% hover:bg-[center_-6px] h-min mr-1 pr-[1px] items-center">
+          <a
+            className="text-black-200 font-inter font-normal text-2xl"
+            href="#"
+          >
+            Hire Me
+          </a>
         </div>
       </div>
     </nav>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,7 +1,14 @@
+import arrowDownRight from '../assets/arrow-down-right.svg';
+
 const Projects = () => {
     return (
-        <section>
-            <h1>Projects</h1>
+        <section className='flex px-2'>
+            <div className='flex flex-col w-1/5'>
+                <h2 className='text-black-200 font-handlee text-[2rem] leading-normal font-semibold'>Featured Projects</h2>
+                <div className='inline-flex justify-end'><img src={arrowDownRight} alt="arrow" /></div>
+                <p className='text-black-200 font-inter text-2xl font-normal leading-8'>Have designed more than 20 projects</p>
+            </div>
+            <div className='w-4/5'></div>
         </section>
     )
 };
