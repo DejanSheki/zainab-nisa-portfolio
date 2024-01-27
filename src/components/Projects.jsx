01d0b3fba31032a16d99f6f9d82b011320fa9f4f
DejanSheki <dejan.sheki.lukic@gmail.com>
2024-01-27 22:35:18 +0100

    ....

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,74 +1,109 @@
-import arrowDownRight from '../assets/arrow-down-right.svg';
-import noCode from '../assets/no-code.png';
-import uiDesign from '../assets/ui-desin.png';
-import uxDesin from '../assets/ux-desin.png';
-import uxDesinApp from '../assets/ux-desin2.png';
-import cursorPink from '../assets/cursor-pink.svg';
-import cursorBlue from '../assets/cursor-blue.svg';
-import cursorYellow from '../assets/cursor-yellow.svg';
-import cursorGreen from '../assets/cursor-green.svg';
+import arrowDownRight from "../assets/arrow-down-right.svg";
+import noCode from "../assets/no-code.png";
+import uiDesign from "../assets/ui-desin.png";
+import uxDesin from "../assets/ux-desin.png";
+import uxDesinApp from "../assets/ux-desin2.png";
+import cursorPink from "../assets/cursor-pink.svg";
+import cursorBlue from "../assets/cursor-blue.svg";
+import cursorYellow from "../assets/cursor-yellow.svg";
+import cursorGreen from "../assets/cursor-green.svg";
 
 const Projects = () => {
-    return (
-        <section className='grid grid-flow-row grid-cols-[16rem_repeat(2,minmax(33%,_1fr))] gap-8 px-3 mb-36'>
-            <div className='row-span-4 col-span-1 flex flex-col mt-[7.5rem] mb-36'>
-                <h2 className='text-black-200 font-handlee text-shadow shadow-black-200 text-[2rem] leading-normal font-semibold bg-pink-100 w-max'>Featured Projects</h2>
-                <div className='inline-flex justify-end my-6 pr-4'><img src={arrowDownRight} alt="arrow" /></div>
-                <p className='text-black-200 font-inter text-2xl font-normal leading-8'>Have designed more than 20 projects</p>
-            </div>
-            <div className='row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-5 pt-5'>
-                <div>
-                    <img src={noCode} alt="No-code" />
-                </div>
-                <div className='flex gap-9 pb-7 mt-11'>
-                    <h3 className='text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-[1.75rem] leading-7 flex-1'>4 style Portfolio design</h3>
-                    <div className='relative'>
-                        <img className='absolute -top-6 -left-5' src={cursorPink} alt="cursor icon" />
-                        <button className='flex py-[0.55944rem] pr-[1.3985rem] pl-[1.11881rem] rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-pink-500 bg-pink-200 shadow-[4.475px_4.475px_11.188px_0px_rgba(247,144,9,0.16)] text-white font-inter text-lg font-medium leading-[1.678rem]'>No-Code</button>
-                    </div>
-
-                </div>
-            </div>
-            <div className='row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-5 pt-5'>
-                <div>
-                    <img src={uiDesign} alt="UI-design" />
-                </div>
-                <div className='flex gap-9 pb-7 mt-11'>
-                    <h3 className='text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-[1.75rem] leading-7 flex-1'>Color system for app</h3>
-                    <div className='relative'>
-                        <img className='absolute -top-6 -left-5' src={cursorBlue} alt="cursor icon" />
-                        <button className='flex py-[0.55944rem] pr-[1.3985rem] pl-[1.11881rem] rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-blue-500 bg-blue-300 shadow-[4.475px_4.475px_11.188px_0px_rgba(9,33,247,0.16)] text-white font-inter text-lg font-medium leading-[1.678rem]'>UI Design</button>
-                    </div>
-
-                </div>
-            </div>
-            <div className='row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-5 pt-5'>
-                <div>
-                    <img src={uxDesin} alt="UX-design" />
-                </div>
-                <div className='flex gap-9 pb-7 mt-11'>
-                    <h3 className='text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-[1.75rem] leading-7 flex-1'>Onboarding screen process</h3>
-                    <div className='relative'>
-                        <img className='absolute -top-6 -left-5' src={cursorYellow} alt="cursor icon" />
-                        <button className='flex py-[0.55944rem] pr-[1.3985rem] pl-[1.11881rem] rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-yellow-400 bg-yellow-200 shadow-[4.475px_4.475px_11.188px_0px_rgba(229,193,65,0.16)] text-white font-inter text-lg font-medium leading-[1.678rem]'>UX Design</button>
-                    </div>
-
-                </div>
-            </div>
-            <div className='row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-5 pt-5'>
-                <div>
-                    <img src={uxDesinApp} alt="UX-desin" />
-                </div>
-                <div className='flex gap-9 pb-7 mt-11'>
-                    <h3 className='text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-[1.75rem] leading-7 flex-1'>Finance Landing page</h3>
-                    <div className='relative'>
-                        <img className='absolute -top-6 -left-5' src={cursorGreen} alt="cursor icon" />
-                        <button className='flex py-[0.55944rem] pr-[1.3985rem] pl-[1.11881rem] rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-green bg-lihgt-green shadow-[4.475px_4.475px_11.188px_0px_rgba(9,33,247,0.16)] text-white font-inter text-lg font-medium leading-[1.678rem]'>UI Design</button>
-                    </div>
-                </div>
-            </div>
-        </section>
-    );
+  return (
+    <section className="grid grid-flow-row grid-cols-1 2xl:grid-cols-[16rem_repeat(2,minmax(33%,_1fr))] gap-8 px-3 mb-36">
+      <div className="row-span-4 col-span-1 flex flex-col  2xl:mt-[7.5rem] bottom-8 2xl:mb-36">
+        <h2 className="text-black-200 font-handlee text-shadow shadow-black-200 text-xl 2xl:text-[2rem] leading-normal font-semibold bg-pink-100 w-max">
+          Featured Projects
+        </h2>
+        <div className="inline-flex justify-start 2xl:justify-end my-4 2xl:my-6 pr-4">
+          <img className="rotate-[50.7deg]" src={arrowDownRight} alt="arrow" />
+        </div>
+        <p className="text-black-200 font-inter text-base 2xl:text-2xl font-normal leading-[125%] 2xl:leading-[140%] w-3/5 2xl:w-full">
+          Have designed more than 20 projects
+        </p>
+      </div>
+      <div className="row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-4 2xl:px-5 pt-5 2xl:pt-5">
+        <div className="h-[70%] 2xl:h-auto">
+          <img src={noCode} alt="No-code" />
+        </div>
+        <div className="flex gap-9 pb-7 mt-11">
+          <h3 className="text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-[1.75rem] leading-7 flex-1">
+            4 style Portfolio design
+          </h3>
+          <div className="relative">
+            <img
+              className="absolute -top-6 -left-5"
+              src={cursorPink}
+              alt="cursor icon"
+            />
+            <button className="flex py-[0.55944rem] pr-[1.3985rem] pl-[1.11881rem] rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-pink-500 bg-pink-200 shadow-[4.475px_4.475px_11.188px_0px_rgba(247,144,9,0.16)] text-white font-inter text-lg font-medium leading-[1.678rem]">
+              No-Code
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-5 pt-5">
+        <div>
+          <img src={uiDesign} alt="UI-design" />
+        </div>
+        <div className="flex gap-9 pb-7 mt-11">
+          <h3 className="text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-[1.75rem] leading-7 flex-1">
+            Color system for app
+          </h3>
+          <div className="relative">
+            <img
+              className="absolute -top-6 -left-5"
+              src={cursorBlue}
+              alt="cursor icon"
+            />
+            <button className="flex py-[0.55944rem] pr-[1.3985rem] pl-[1.11881rem] rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-blue-500 bg-blue-300 shadow-[4.475px_4.475px_11.188px_0px_rgba(9,33,247,0.16)] text-white font-inter text-lg font-medium leading-[1.678rem]">
+              UI Design
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-5 pt-5">
+        <div>
+          <img src={uxDesin} alt="UX-design" />
+        </div>
+        <div className="flex gap-9 pb-7 mt-11">
+          <h3 className="text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-[1.75rem] leading-7 flex-1">
+            Onboarding screen process
+          </h3>
+          <div className="relative">
+            <img
+              className="absolute -top-6 -left-5"
+              src={cursorYellow}
+              alt="cursor icon"
+            />
+            <button className="flex py-[0.55944rem] pr-[1.3985rem] pl-[1.11881rem] rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-yellow-400 bg-yellow-200 shadow-[4.475px_4.475px_11.188px_0px_rgba(229,193,65,0.16)] text-white font-inter text-lg font-medium leading-[1.678rem]">
+              UX Design
+            </button>
+          </div>
+        </div>
+      </div>
+      <div className="row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-5 pt-5">
+        <div>
+          <img src={uxDesinApp} alt="UX-desin" />
+        </div>
+        <div className="flex gap-9 pb-7 mt-11">
+          <h3 className="text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-[1.75rem] leading-7 flex-1">
+            Finance Landing page
+          </h3>
+          <div className="relative">
+            <img
+              className="absolute -top-6 -left-5"
+              src={cursorGreen}
+              alt="cursor icon"
+            />
+            <button className="flex py-[0.55944rem] pr-[1.3985rem] pl-[1.11881rem] rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-green bg-lihgt-green shadow-[4.475px_4.475px_11.188px_0px_rgba(9,33,247,0.16)] text-white font-inter text-lg font-medium leading-[1.678rem]">
+              UI Design
+            </button>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Projects;
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -5,7 +5,7 @@ import rhombus from "../assets/rhombus.svg";
 
 const Services = () => {
   return (
-    <section className="my-[4.69rem] 2xl:mb-36 2xl:mt-[6.875rem] px-[1.19rem] 2xl:px-0">
+    <section className="my-[4.69rem] pb-[4.69rem] 2xl:pb-0 2xl:mb-36 2xl:mt-[6.875rem] px-[1.19rem] 2xl:px-0">
       <div className="2xl:pl-14">
         <h2 className="font-handlee text-black-200 text-shadow shadow-black-200 leading-normal font-semibold text-xl 2xl:text-[2rem] w-max bg-pink-100">
           What I do?
@@ -33,7 +33,7 @@ const Services = () => {
             UI & Product Design
           </h3>
         </div>
-        <div className='flex flex-col items-start justify-center relative gap-6 2xl:gap-9 w-[16.25rem] 2xl:w-[23.6rem] h-[19.44rem] 2xl:h-[28.25rem] -rotate-[5deg] rounded-lg bg-pink-100 border-pink-300 border-solid border-[5px] shadow-[5px_5px_20px_0px_rgba(255,201,240,0.13)] px-9 2xl:px-14 pt-16 translate-x-5 2xl:translate-x-16 translate-y-[3.1rem] 2xl:translate-y-1 before:content-["Webflow"] before:bg-yellow-100 before:text-black-200 before:font-handlee before:text-2xl 2xl:before:text-[2rem] before:leading-normal before:font-semibold before:pr-[3px] before:absolute before:top-8 2xl:before:top-24 before:-left-4 2xl:before:-right-8 before:rotate-[5.1deg]'>
+        <div className='flex flex-col items-start justify-center relative gap-6 2xl:gap-9 w-[16.25rem] 2xl:w-[23.6rem] h-[19.44rem] 2xl:h-[28.25rem] -rotate-[5deg] rounded-lg bg-pink-100 border-pink-300 border-solid border-[5px] shadow-[5px_5px_20px_0px_rgba(255,201,240,0.13)] px-9 2xl:px-14 pt-16 translate-x-5 2xl:translate-x-16 translate-y-[3.1rem] 2xl:translate-y-1 before:content-["Webflow"] before:bg-yellow-100 before:text-black-200 before:font-handlee before:text-2xl 2xl:before:text-[2rem] before:leading-normal before:font-semibold before:pr-[3px] before:absolute before:top-8 2xl:before:top-24 before:right-[10.8rem] 2xl:before:-right-6 before:rotate-[5.1deg]'>
           <img
             className="w-[2.7rem] 2xl:w-[3.375rem]"
             src={rhombus}
