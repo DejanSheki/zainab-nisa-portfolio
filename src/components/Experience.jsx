0af959c053413b92d4ef3f94432d533ddcee0871
DejanSheki <dejan.sheki.lukic@gmail.com>
2024-01-28 18:17:00 +0100

    ....

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -2,74 +2,74 @@ import arrowDownRight from "../assets/arrow-down-right.svg";
 
 const Experience = () => {
   return (
-    <section className="flex justify-between px-1 mb-36">
-      <div className="flex flex-col w-64 mt-20">
-        <h2 className="text-black-200 font-handlee text-shadow shadow-black-200 text-[2rem] leading-normal font-semibold bg-pink-100 w-max">
+    <section className="flex flex-col 2xl:flex-row justify-between px-3 pr-1 2xl:px-1 mb-[4.69rem] 2xl:mb-36">
+      <div className="flex flex-col w-64 2xl:mt-20">
+        <h2 className="text-black-200 font-handlee text-shadow shadow-black-200 text-xl 2xl:text-[2rem] leading-normal font-semibold bg-pink-100 w-max">
           Work Experience
         </h2>
-        <div className="inline-flex justify-end my-6 pr-4">
-          <img src={arrowDownRight} alt="arrow" />
+        <div className="inline-flex justify-start 2xl:justify-end my-4 2xl:my-6 pr-4">
+          <img className="rotate-[50.7deg]" src={arrowDownRight} alt="arrow" />
         </div>
-        <p className="text-black-200 font-inter text-2xl font-normal leading-8">
+        <p className="text-black-200 font-inter text-base 2xl:text-2xl font-normal leading-[125%] 2xl:leading-[140%] w-4/5 2xl:w-full">
           Have been designing since my past 4 years
         </p>
       </div>
-      <div className="border-x-2 border-black-100 w-[65%] mr-10 pt-[3.69rem] pb-[5.3rem]">
-        <div className="w-[113%] h-[2px] bg-black-100 -ml-[7.33%]"></div>
-        <div className="flex flex-col gap-[2.31rem] pt-16 pb-[4.83rem] pl-[4.44rem] pr-[7.38rem]">
-          <div className="flex gap-[2.312rem]">
-            <div className="text-black-100 text-shadow-lg shadow-black-100 text-center font-handlee text-5xl font-semibold leading-[150%] tracking-[-0.066rem] py-2 px-[2.437rem] rounded-md border-2 border-yellow-300 bg-yellow-100">
+      <div className="2xl:border-x-2 2xl:border-black-100 w-full 2xl:w-[65%] 2xl:mr-10 2xl:pt-[3.69rem] 2xl:pb-[5.3rem]">
+        <div className="2xl:w-[113%] 2xl:h-[2px] 2xl:bg-black-100 2xl:-ml-[7.33%]"></div>
+        <div className="flex flex-col gap-5 2xl:gap-[2.31rem] pt-8 2xl:pt-16 2xl:pb-[4.83rem] 2xl:pl-[4.44rem] 2xl:pr-[7.38rem]">
+          <div className="flex gap-3 2xl:gap-[2.312rem]">
+            <div className="text-black-100 text-shadow-lg shadow-black-100 text-center font-handlee text-[1.9rem] 2xl:text-5xl font-semibold leading-[150%] 2xl:leading-[150%] tracking-[-0.052rem] 2xl:tracking-[-0.066rem] py-[0.32rem] 2xl:py-2 px-[1.55rem] 2xl:px-[2.437rem] rounded-md border-2 border-yellow-300 bg-yellow-100">
               1
             </div>
             <div>
-              <h3 className="text-black-200 font-inter text-2xl font-normal leading-[140%] mb-[0.38rem]">
+              <h3 className="text-black-200 font-inter text-base 2xl:text-2xl font-normal leading-[140%] 2xl:leading-[140%] mb-[0.38rem]">
                 Design intern at
                 <span className="font-bold"> Google</span>
               </h3>
-              <p className="font-inter text-black-200 text-lg font-normal leading-[140%]">
+              <p className="font-inter text-black-200 text-[0.69rem] 2xl:text-lg font-normal leading-[140%] 2xl:leading-[140%]">
                 Worked on design system at Material 3 designs
               </p>
-              <p className="text-grey text-base font-medium leading-[140%] mt-[0.1875rem]">
+              <p className="text-grey text-[0.6rem] 2xl:text-base font-medium leading-[140%] mt-[0.1875rem]">
                 20, April 2021
               </p>
             </div>
           </div>
-          <div className="flex gap-[2.31rem]">
-            <div className="text-black-100 text-shadow-lg shadow-black-100 text-center font-handlee text-5xl font-semibold leading-[150%] tracking-[-0.066rem] py-2 px-[2.0921rem] rounded-md border-2 border-blue-300 bg-blue-200">
+          <div className="flex gap-3 2xl:gap-[2.312rem]">
+            <div className="text-black-100 text-shadow-lg shadow-black-100 text-center font-handlee text-[1.9rem] 2xl:text-5xl font-semibold leading-[150%] 2xl:leading-[150%] tracking-[-0.041rem] 2xl:tracking-[-0.066rem] py-[0.32rem] 2xl:py-2 px-[1.32rem] 2xl:px-[2.0921rem] rounded-md border-2 border-blue-300 bg-blue-200">
               2
             </div>
             <div>
-              <h3 className="text-black-200 font-inter text-2xl font-normal leading-[140%] mb-[0.38rem]">
+              <h3 className="text-black-200 font-inter text-base 2xl:text-2xl font-normal leading-[140%] mb-[0.38rem]">
                 Sr. UI/UX Designer at
                 <span className="font-bold"> Microsoft</span>
               </h3>
-              <p className="font-inter text-black-200 text-lg font-normal leading-[140%]">
+              <p className="font-inter text-black-200 text-[0.69rem] 2xl:text-lg font-normal leading-[140%] 2xl:leading-[140%]">
                 Worked on design system at Material 3 designs
               </p>
-              <p className="text-grey text-base font-medium leading-[140%] mt-[0.1875rem]">
+              <p className="text-grey text-[0.6rem] 2xl:text-base font-medium leading-[140%] mt-[0.1875rem]">
                 20, April 2022
               </p>
             </div>
           </div>
-          <div className="flex gap-[2.31rem]">
-            <div className="text-black-100 text-center text-shadow-lg shadow-black-100 font-handlee text-5xl font-semibold leading-[150%] tracking-[-0.066rem] py-2 px-[2.0921rem] rounded-md border-2 border-pink-300 bg-pink-100">
+          <div className="flex gap-3 2xl:gap-[2.312rem]">
+            <div className="text-black-100 text-center text-shadow-lg shadow-black-100 font-handlee text-[1.9rem] 2xl:text-5xl font-semibold leading-[150%] 2xl:leading-[150%] tracking-[-0.041rem] 2xl:tracking-[-0.066rem] py-[0.32rem] 2xl:py-2 px-[1.356rem] 2xl:px-[2.1621rem] rounded-md border-2 border-pink-300 bg-pink-100">
               3
             </div>
             <div>
-              <h3 className="text-black-200 font-inter text-2xl font-normal leading-[140%] mb-[0.38rem]">
+              <h3 className="text-black-200 font-inter text-base 2xl:text-2xl font-normal leading-[140%] mb-[0.38rem]">
                 Software Engineer at
                 <span className="font-bold"> SASS Startup</span>
               </h3>
-              <p className="font-inter text-black-200 text-lg font-normal leading-[140%]">
+              <p className="font-inter text-black-200 text-[0.69rem] 2xl:text-lg font-normal leading-[140%] 2xl:leading-[140%]">
                 Worked on design system at Material 3 designs
               </p>
-              <p className="text-grey text-base font-medium leading-[140%] mt-[0.1875rem]">
+              <p className="text-grey text-[0.6rem] 2xl:text-base font-medium leading-[140%] mt-[0.1875rem]">
                 20, April 2023
               </p>
             </div>
           </div>
         </div>
-        <div className="w-[113%] h-[2px] bg-black-100 -ml-[7.33%]"></div>
+        <div className="2xl:w-[113%] 2xl:h-[2px] 2xl:bg-black-100 2xl:-ml-[7.33%]"></div>
       </div>
     </section>
   );
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -10,7 +10,7 @@ import cursorGreen from "../assets/cursor-green.svg";
 
 const Projects = () => {
   return (
-    <section className="grid grid-flow-row grid-cols-1 2xl:grid-cols-[16rem_repeat(2,minmax(33%,_1fr))] gap-8 px-3 mb-36">
+    <section className="grid grid-flow-row grid-cols-1 2xl:grid-cols-[16rem_repeat(2,minmax(33%,_1fr))] gap-8 px-3 mb-[4.69rem] 2xl:mb-36">
       <div className="row-span-4 col-span-1 flex flex-col  2xl:mt-[7.5rem] bottom-8 2xl:mb-36">
         <h2 className="text-black-200 font-handlee text-shadow shadow-black-200 text-xl 2xl:text-[2rem] leading-normal font-semibold bg-pink-100 w-max">
           Featured Projects
@@ -22,81 +22,81 @@ const Projects = () => {
           Have designed more than 20 projects
         </p>
       </div>
-      <div className="row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-4 2xl:px-5 pt-5 2xl:pt-5">
-        <div className="h-[70%] 2xl:h-auto">
+      <div className="row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-4 2xl:px-5 pt-4 2xl:pt-5">
+        <div>
           <img src={noCode} alt="No-code" />
         </div>
-        <div className="flex gap-9 pb-7 mt-11">
-          <h3 className="text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-[1.75rem] leading-7 flex-1">
+        <div className="flex items-end gap-3 2xl:gap-9 pb-4 2xl:pb-7 mt-4 2xl:mt-11">
+          <h3 className="text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-xl 2xl:text-[1.75rem] leading-5 2xl:leading-7 flex-1">
             4 style Portfolio design
           </h3>
           <div className="relative">
             <img
-              className="absolute -top-6 -left-5"
+              className="absolute w-4 2xl:w-auto -top-3 2xl:-top-6 -left-3 2xl:-left-5"
               src={cursorPink}
               alt="cursor icon"
             />
-            <button className="flex py-[0.55944rem] pr-[1.3985rem] pl-[1.11881rem] rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-pink-500 bg-pink-200 shadow-[4.475px_4.475px_11.188px_0px_rgba(247,144,9,0.16)] text-white font-inter text-lg font-medium leading-[1.678rem]">
+            <button className="flex py-[0.41rem] 2xl:py-[0.55944rem] pr-4 2xl:pr-[1.3985rem] pl-[0.82rem] 2xl:pl-[1.11881rem] rounded-[0.10294rem_1.23506rem_1.23506rem_1.23506rem] 2xl:rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-pink-500 bg-pink-200 shadow-[4.475px_4.475px_11.188px_0px_rgba(247,144,9,0.16)] text-white font-inter text-[0.82rem] 2xl:text-lg font-medium leading-5 2xl:leading-[1.678rem]">
               No-Code
             </button>
           </div>
         </div>
       </div>
-      <div className="row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-5 pt-5">
+      <div className="row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-4 2xl:px-5 pt-4 2xl:pt-5">
         <div>
           <img src={uiDesign} alt="UI-design" />
         </div>
-        <div className="flex gap-9 pb-7 mt-11">
-          <h3 className="text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-[1.75rem] leading-7 flex-1">
+        <div className="flex items-end gap-3 2xl:gap-9 pb-4 2xl:pb-7 mt-4 2xl:mt-11">
+          <h3 className="text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-xl 2xl:text-[1.75rem] leading-5 2xl:leading-7 flex-1">
             Color system for app
           </h3>
           <div className="relative">
             <img
-              className="absolute -top-6 -left-5"
+              className="absolute w-4 2xl:w-auto -top-3 2xl:-top-6 -left-3 2xl:-left-5"
               src={cursorBlue}
               alt="cursor icon"
             />
-            <button className="flex py-[0.55944rem] pr-[1.3985rem] pl-[1.11881rem] rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-blue-500 bg-blue-300 shadow-[4.475px_4.475px_11.188px_0px_rgba(9,33,247,0.16)] text-white font-inter text-lg font-medium leading-[1.678rem]">
+            <button className="flex py-[0.41rem] 2xl:py-[0.55944rem] pr-4 2xl:pr-[1.3985rem] pl-[0.82rem] 2xl:pl-[1.11881rem] rounded-[0.10294rem_1.23506rem_1.23506rem_1.23506rem] 2xl:rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-blue-500 bg-blue-300 shadow-[4.475px_4.475px_11.188px_0px_rgba(9,33,247,0.16)] text-white font-inter text-[0.82rem] 2xl:text-lg font-medium leading-5 2xl:leading-[1.678rem]">
               UI Design
             </button>
           </div>
         </div>
       </div>
-      <div className="row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-5 pt-5">
+      <div className="row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-4 2xl:px-5 pt-4 2xl:pt-5">
         <div>
           <img src={uxDesin} alt="UX-design" />
         </div>
-        <div className="flex gap-9 pb-7 mt-11">
-          <h3 className="text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-[1.75rem] leading-7 flex-1">
+        <div className="flex gap-3 2xl:gap-9 pb-4 2xl:pb-7 mt-4 2xl:mt-11">
+          <h3 className="text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-xl 2xl:text-[1.75rem] leading-5 2xl:leading-7 flex-1">
             Onboarding screen process
           </h3>
           <div className="relative">
             <img
-              className="absolute -top-6 -left-5"
+              className="absolute w-4 2xl:w-auto -top-3 2xl:-top-6 -left-3 2xl:-left-5"
               src={cursorYellow}
               alt="cursor icon"
             />
-            <button className="flex py-[0.55944rem] pr-[1.3985rem] pl-[1.11881rem] rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-yellow-400 bg-yellow-200 shadow-[4.475px_4.475px_11.188px_0px_rgba(229,193,65,0.16)] text-white font-inter text-lg font-medium leading-[1.678rem]">
+            <button className="flex py-[0.41rem] 2xl:py-[0.55944rem] pr-4 2xl:pr-[1.3985rem] pl-[0.82rem] 2xl:pl-[1.11881rem] rounded-[0.10294rem_1.23506rem_1.23506rem_1.23506rem] 2xl:rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-yellow-400 bg-yellow-200 shadow-[4.475px_4.475px_11.188px_0px_rgba(229,193,65,0.16)] text-white font-inter text-[0.82rem] 2xl:text-lg font-medium leading-5 2xl:leading-[1.678rem]">
               UX Design
             </button>
           </div>
         </div>
       </div>
-      <div className="row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-5 pt-5">
+      <div className="row-span-2 col-span-1 flex flex-col bg-white border-solid border-black-100 border-[3px] rounded-md px-4 2xl:px-5 pt-4 2xl:pt-5">
         <div>
           <img src={uxDesinApp} alt="UX-desin" />
         </div>
-        <div className="flex gap-9 pb-7 mt-11">
-          <h3 className="text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-[1.75rem] leading-7 flex-1">
+        <div className="flex gap-3 2xl:gap-9 pb-4 2xl:pb-7 mt-4 2xl:mt-11">
+          <h3 className="text-black-200 font-semibold text-shadow shadow-black-200 font-handlee text-xl 2xl:text-[1.75rem] leading-5 2xl:leading-7 flex-1">
             Finance Landing page
           </h3>
           <div className="relative">
             <img
-              className="absolute -top-6 -left-5"
+              className="absolute w-4 2xl:w-auto -top-3 2xl:-top-6 -left-3 2xl:-left-5"
               src={cursorGreen}
               alt="cursor icon"
             />
-            <button className="flex py-[0.55944rem] pr-[1.3985rem] pl-[1.11881rem] rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-green bg-lihgt-green shadow-[4.475px_4.475px_11.188px_0px_rgba(9,33,247,0.16)] text-white font-inter text-lg font-medium leading-[1.678rem]">
+            <button className="flex py-[0.41rem] 2xl:py-[0.55944rem] pr-4 2xl:pr-[1.3985rem] pl-[0.82rem] 2xl:pl-[1.11881rem] rounded-[0.10294rem_1.23506rem_1.23506rem_1.23506rem] 2xl:rounded-[0.13988rem_1.67825rem_1.67825rem_1.67825rem] border-[2.238px] border-solid border-green bg-lihgt-green shadow-[4.475px_4.475px_11.188px_0px_rgba(9,33,247,0.16)] text-white font-inter text-[0.82rem] 2xl:text-lg font-medium leading-5 2xl:leading-[1.678rem]">
               UI Design
             </button>
           </div>
